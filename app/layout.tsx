770421888afd0ac9b0b6e2d4c35bca77a2727ab9
Kriti Agarwal <kriti.agarwal4709@gmail.com>
2024-02-01 15:03:49 +0000

    Authentication modal and functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Sidebar from "@/components/sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,13 @@ import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+
+import useAuthModel from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
 import Button from "./button";
 
 interface HeaderProps {
@@ -13,10 +20,22 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, classname }) => {
+  const authModel = useAuthModel();
   const router = useRouter();
 
-  const handleLogout = () => {
-    // hanlde log out
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // TODO: Reset any playing songs
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('Logged out');
+    }
   };
 
   return (
@@ -71,42 +90,54 @@ const Header: React.FC<HeaderProps> = ({ children, classname }) => {
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-            <button
-                className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                <HiHome className="text-black" size={20} />
-            </button>
-            <button
-                className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                <BiSearch className="text-black" size={20} />
-            </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push("/account")}
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
             <>
-                <div>
-                    <Button
-                        onClick={() => {}}
-                        className="
+              <div>
+                <Button
+                  onClick={authModel.onOpen}
+                  className="
                             bg-transparent
                             text-neutral-300
                             font-medium
                         "
-                    >
-                        Sign up
-                    </Button>
-                </div>
-                <div>
-                    <Button
-                        onClick={() => {}}
-                        className="
+                >
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModel.onOpen}
+                  className="
                             bg-white
                             px-6
                             py-2
                         "
-                    >
-                        Log in
-                    </Button>
-                </div>
+                >
+                  Log in
+                </Button>
+              </div>
             </>
+          )}
         </div>
       </div>
       {children}
